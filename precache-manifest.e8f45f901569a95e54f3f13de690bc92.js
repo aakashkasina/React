6ff0b4da54f6fc25065b2706e11bd4b794a10607
aakashkasina <52420873+aakashkasina@users.noreply.github.com>
2019-07-01 22:57:53 +0530

    Updates

---
+++ precache-manifest.e8f45f901569a95e54f3f13de690bc92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1750be5a937e9eee8d5df98e665bd4d6",
+    "url": "/aakashkasina/samplewebsite/index.html"
+  },
+  {
+    "revision": "50c8cbe2295bc50c79f9",
+    "url": "/aakashkasina/samplewebsite/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/aakashkasina/samplewebsite/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "50c8cbe2295bc50c79f9",
+    "url": "/aakashkasina/samplewebsite/static/js/main.ac5c1b8e.chunk.js"
+  },
+  {
+    "revision": "9cb2c3d42fa73f0e6507",
+    "url": "/aakashkasina/samplewebsite/static/js/runtime~main.5f6f728b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aakashkasina/samplewebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
